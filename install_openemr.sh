5fedf91be447aea279c73c5d8b763903fb9c7e77
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-21 17:29:20 -0500

    Update install_openemr.sh

--- install_openemr.sh
+++ install_openemr.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if grep -q Debian "/etc/os-release" ; then
+	echo "Debian is installed"
+	echo
+	echo "Installing Debian prerequisites"
+	echo
+	sudo apt update
+	sudo apt install -y curl wget gnupg2 ca-certificates lsb-release apt-transport-https
+	wget https://packages.sury.org/php/apt.gpg
+	sudo apt-key add apt.gpg
+  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.list
+else
+	echo "Not Debian...continuing"
+fi
+
 echo "Performing updates"
 sudo apt update && sudo apt upgrade -y
 echo
