ee0507d2b6413c01fe4287c03cbf10d6d4bc6eee
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2022-08-09 21:36:50 -0400

    Update install_openemr.sh

--- install_openemr.sh
+++ install_openemr.sh
@@ -38,7 +38,7 @@ echo
 echo "Downloading OpenEMR"
 echo
 wget https://sourceforge.net/projects/openemr/files/latest/download/openemr-6.0.0.tar.gz
-sudo unzip openemr-6.0.0.tar.gz
+sudo unzip openemr-7.0.0.tar.gz
 sudo mv openemr-5.0.1 openemr
 sudo cp -r openemr /var/www/html/
 sudo chown -R www-data: /var/www/html
